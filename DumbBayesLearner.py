68386aeca6417ab3a27dc5817dba1d236661d489
LunasAbacus <lunasabacus@gmail.com>
2013-09-26 22:07:45 -0400

    Skeleton for Dumb Bayes Learner

---
+++ DumbBayesLearner.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        Dumb Bayes Learner
+# Purpose:
+#
+# Author:      Nathan Jacobs
+#
+# Created:     26/09/2013
+# Copyright:   (c) Shiro_Raven 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+def main():
+
+    def ParseFile(filename):
+        pass
+
+    def Learn():
+        pass
+
+    def WriteLearnedWeights():
+        pass
+
+    pass
+
+if __name__ == '__main__':
+    main()
